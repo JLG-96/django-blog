d6e70c4aa5ce7ba81b7bceaf2f78cd557534af8f
Jack Leonard-gill <u0zy2qef@students.codeinstitute.net>
2025-01-26 17:18:32 +0000

    added post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-jlg96-djangoblog-ihb0j1uxz16.ws.codeinstitute-ide.net','.herokuapp.com']
 
